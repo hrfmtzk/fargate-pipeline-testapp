c79b14d8291ddbc98bff9333fbe71e31b8633244
Hirofumi Tsuzuki <hrfm.tsuzuki@gmail.com>
2022-12-13 17:12:39 +0900

    add pipeline resource

--- fargate_pipeline_testapp/fargate_pipeline_testapp_stack.py
+++ fargate_pipeline_testapp/fargate_pipeline_testapp_stack.py
@@ -1,19 +1,72 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_codebuild as codebuild,
+    aws_codecommit as codecommit,
+    aws_codepipeline as codepipeline,
+    aws_codepipeline_actions as cpactions,
+    aws_ecr as ecr,
 )
 from constructs import Construct
 
-class FargatePipelineTestappStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+class FargatePipelineTestappStack(Stack):
+    def __init__(
+        self,
+        scope: Construct,
+        construct_id: str,
+        target_branch_name: str = "main",
+        **kwargs
+    ) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        pipeline = codepipeline.Pipeline(
+            self,
+            "Pipeline",
+        )
+        source_artifact = codepipeline.Artifact("source_artifact")
+        repository = ecr.Repository(
+            self,
+            "Repository",
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "FargatePipelineTestappQueue",
-        #     visibility_timeout=Duration.seconds(300),
+        pipeline.add_stage(
+            stage_name="Source",
+            actions=[
+                cpactions.CodeCommitSourceAction(
+                    output=source_artifact,
+                    repository=codecommit.Repository(
+                        self,
+                        "Repository",
+                        repository_name="testapp-repository",
+                    ),
+                    branch=target_branch_name,
+                    trigger=cpactions.CodeCommitTrigger.EVENTS,
+                    action_name="Source",
+                ),
+            ],
+        )
+
+        build_project = codebuild.PipelineProject(
+            self,
+            "BuildProject",
+            environment=codebuild.BuildEnvironment(
+                build_image=codebuild.LinuxBuildImage.STANDARD_6_0,
+            ),
+        )
+        repository.grant_pull_push(build_project)
+        pipeline.add_stage(
+            stage_name="Build",
+            actions=[
+                cpactions.CodeBuildAction(
+                    input=source_artifact,
+                    project=build_project,
+                ),
+            ],
+        )
+
+        # pipeline.add_stage(
+        #     stage_name="Deploy",
+        #     actions=[
+        #         cpactions.EcsDeployAction(),
+        #     ],
         # )
